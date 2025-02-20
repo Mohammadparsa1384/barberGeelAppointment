258de3a2f7e0bce9d1ce1f105e38f917fee24b20
Parsa Javidi <parsajavidi17@gmail.com>
2025-02-21 01:07:28 +0330

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path("signup/", views.SignUpView.as_view(),name="signup"),
-    path("login/", views.CustomLoginView.as_view(), name="login"),
-]
